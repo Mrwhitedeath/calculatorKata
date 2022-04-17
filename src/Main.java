e4f17aceb4f479743925ae26657856e1ba605922
Mrwhitedeath <belovsergey@mail.ru>
2022-04-17 10:49:51 +0300

    add method isRoman

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,35 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Numbers numbers = new Numbers();
+
         System.out.println("Введите уравнение");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
 
         String[] calcInput = input.split(" ");
 
+        System.out.println(calcInput.length);
+
         System.out.println(calcInput[0] + " _ " + calcInput[1] + " _ " + calcInput[2]);
 
-
-
-        int result = 0;
-
-
+        if (numbers.isArabic(calcInput[0]) & numbers.isArabic(calcInput[2])){
+            int result;
+            result = numbers.calculate(Integer.parseInt(calcInput[0]), Integer.parseInt(calcInput[2]), calcInput[1]);
+            System.out.println("Результат: " + result);
+        } else if (numbers.isRoman(calcInput[0]) & numbers.isRoman(calcInput[0])){
+        } else {
+            System.out.println("Некорректное выражение");
+        }
     }
 }
 
-class numbers{
+class Numbers{
 
-    private boolean isArabic(String num){
+     boolean isArabic(String num){
         boolean result;
         try {
             int digit = Integer.parseInt(num);
@@ -31,7 +40,11 @@ class numbers{
         return result;
     }
 
-    private int calculate(int num1, int num2, String operation){
+    boolean isRoman(String num){
+         return num.matches("[XVI]{1,4}");
+    }
+
+    int calculate(int num1, int num2, @NotNull String operation){
         int result = 0;
         switch (operation){
             case ("+") -> result = num1 + num2;
